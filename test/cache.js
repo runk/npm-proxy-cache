cae3802c33268dd84606c32e10d9e3165f7ef96d
Dmitry Shirokov <deadrunk@gmail.com>
2017-04-12 10:47:46 +1000

    linter

--- test/cache.js
+++ test/cache.js
@@ -1,22 +1,23 @@
+'use strict';
 var assert = require('assert'),
+  path = require('path'),
   rimraf = require('rimraf'),
-  fs = require('fs'),
   Cache = require('../lib/cache');
 
 describe('cache', function() {
 
   var opts;
-  var path = __dirname + '/cache';
+  var filepath = path.join(__dirname, '/cache');
   beforeEach(function() {
-    opts = {path: path, ttl: 10};
+    opts = {path: filepath, ttl: 10};
   });
 
   before(function(done) {
-    rimraf(path, done);
+    rimraf(filepath, done);
   });
 
   after(function(done) {
-    rimraf(path, done);
+    rimraf(filepath, done);
   });
 
 
@@ -50,7 +51,7 @@ describe('cache', function() {
       });
 
       readable.read();
-    })
+    });
   });
 
 
@@ -80,14 +81,14 @@ describe('cache', function() {
   describe('getPath()', function() {
     it('return path info', function() {
       var cache = new Cache(opts);
-      var path = cache.getPath('/foo/bar/-/../baz.tgz');
-      assert.equal(path.dir, opts.path + '/f/a/7');
-      assert.equal(path.file, 'fa7bf9eb.tgz');
-      assert.equal(path.full, opts.path + '/f/a/7/fa7bf9eb.tgz');
-      assert.equal(path.rel, 'f/a/7/fa7bf9eb.tgz');
+      var filepath = cache.getPath('/foo/bar/-/../baz.tgz');
+      assert.equal(filepath.dir, opts.path + '/f/a/7');
+      assert.equal(filepath.file, 'fa7bf9eb.tgz');
+      assert.equal(filepath.full, opts.path + '/f/a/7/fa7bf9eb.tgz');
+      assert.equal(filepath.rel, 'f/a/7/fa7bf9eb.tgz');
     });
 
-    describe ('given the friendlyNames option is set', function () {
+    describe ('given the friendlyNames option is set', function() {
       var cache;
 
       beforeEach(function() {
@@ -95,52 +96,52 @@ describe('cache', function() {
         cache = new Cache(opts);
       });
 
-      it('uses just the module name from the URL', function () {
-        var path = cache.getPath('http://registry/test');
-        assert.equal(path.dir, opts.path + '/t/e/s');
-        assert.equal(path.file, 'test');
-        assert.equal(path.full, opts.path + '/t/e/s/test');
-        assert.equal(path.rel, 't/e/s/test');
+      it('uses just the module name from the URL', function() {
+        var filepath = cache.getPath('http://registry/test');
+        assert.equal(filepath.dir, opts.path + '/t/e/s');
+        assert.equal(filepath.file, 'test');
+        assert.equal(filepath.full, opts.path + '/t/e/s/test');
+        assert.equal(filepath.rel, 't/e/s/test');
       });
 
-      it('cuts the file extension from the module URL', function () {
-        var path = cache.getPath('http://registry/test.tgz');
-        assert.equal(path.dir, opts.path + '/t/e/s');
-        assert.equal(path.file, 'test.tgz');
-        assert.equal(path.full, opts.path + '/t/e/s/test.tgz');
-        assert.equal(path.rel, 't/e/s/test.tgz');
+      it('cuts the file extension from the module URL', function() {
+        var filepath = cache.getPath('http://registry/test.tgz');
+        assert.equal(filepath.dir, opts.path + '/t/e/s');
+        assert.equal(filepath.file, 'test.tgz');
+        assert.equal(filepath.full, opts.path + '/t/e/s/test.tgz');
+        assert.equal(filepath.rel, 't/e/s/test.tgz');
       });
 
-      it('cuts the version suffix from the module URL', function () {
-        var path = cache.getPath('http://registry/test-1.2.3.tgz');
-        assert.equal(path.dir, opts.path + '/t/e/s');
-        assert.equal(path.file, 'test-1.2.3.tgz');
-        assert.equal(path.full, opts.path + '/t/e/s/test-1.2.3.tgz');
-        assert.equal(path.rel, 't/e/s/test-1.2.3.tgz');
+      it('cuts the version suffix from the module URL', function() {
+        var filepath = cache.getPath('http://registry/test-1.2.3.tgz');
+        assert.equal(filepath.dir, opts.path + '/t/e/s');
+        assert.equal(filepath.file, 'test-1.2.3.tgz');
+        assert.equal(filepath.full, opts.path + '/t/e/s/test-1.2.3.tgz');
+        assert.equal(filepath.rel, 't/e/s/test-1.2.3.tgz');
       });
 
-      it('uses hyphens instead of dots in the directory structure', function () {
-        var path = cache.getPath('http://registry/te.st');
-        assert.equal(path.dir, opts.path + '/t/e/-');
-        assert.equal(path.file, 'te.st');
-        assert.equal(path.full, opts.path + '/t/e/-/te.st');
-        assert.equal(path.rel, 't/e/-/te.st');
+      it('uses hyphens instead of dots in the directory structure', function() {
+        var filepath = cache.getPath('http://registry/te.st');
+        assert.equal(filepath.dir, opts.path + '/t/e/-');
+        assert.equal(filepath.file, 'te.st');
+        assert.equal(filepath.full, opts.path + '/t/e/-/te.st');
+        assert.equal(filepath.rel, 't/e/-/te.st');
       });
 
-      it('uses short direcory structure for short module name', function () {
-        var path = cache.getPath('http://registry/q');
-        assert.equal(path.dir, opts.path + '/q/-/-');
-        assert.equal(path.file, 'q');
-        assert.equal(path.full, opts.path + '/q/-/-/q');
-        assert.equal(path.rel, 'q/-/-/q');
+      it('uses short direcory structure for short module name', function() {
+        var filepath = cache.getPath('http://registry/q');
+        assert.equal(filepath.dir, opts.path + '/q/-/-');
+        assert.equal(filepath.file, 'q');
+        assert.equal(filepath.full, opts.path + '/q/-/-/q');
+        assert.equal(filepath.rel, 'q/-/-/q');
       });
 
-      it('cuts the version suffix and file extension from short module names', function () {
-        var path = cache.getPath('http://registry/q-1.2.3.tgz');
-        assert.equal(path.dir, opts.path + '/q/-/-');
-        assert.equal(path.file, 'q-1.2.3.tgz');
-        assert.equal(path.full, opts.path + '/q/-/-/q-1.2.3.tgz');
-        assert.equal(path.rel, 'q/-/-/q-1.2.3.tgz');
+      it('cuts the version suffix and file extension from short module names', function() {
+        var filepath = cache.getPath('http://registry/q-1.2.3.tgz');
+        assert.equal(filepath.dir, opts.path + '/q/-/-');
+        assert.equal(filepath.file, 'q-1.2.3.tgz');
+        assert.equal(filepath.full, opts.path + '/q/-/-/q-1.2.3.tgz');
+        assert.equal(filepath.rel, 'q/-/-/q-1.2.3.tgz');
       });
     });
   });
--- test/proxy.js
+++ test/proxy.js
@@ -1,6 +1,5 @@
+'use strict';
 var assert = require('assert'),
-  fs = require('fs'),
-  proxy = require('../lib/proxy'),
   http = require('http'),
   request = require('request'),
   uuid = require('uuid');
@@ -9,15 +8,15 @@ var MOCK_PORT = 0;
 var APP_PORT = 32402;
 var calledMock = {};
 
-var server = http.createServer(function (req, res) {
-    calledMock[req.method + req.url] = calledMock[req.method + req.url] + 1 || 1;
-    res.setHeader('Content-Type', 'application/json');
-    res.end(JSON.stringify({
-      name: uuid().toString()
-    }));
+var server = http.createServer(function(req, res) {
+  calledMock[req.method + req.url] = calledMock[req.method + req.url] + 1 || 1;
+  res.setHeader('Content-Type', 'application/json');
+  res.end(JSON.stringify({
+    name: uuid().toString()
+  }));
 });
 
-before(function (beforeDone) {
+before(function(beforeDone) {
   server.listen(MOCK_PORT, function(err) {
     if (err) {
       return beforeDone(err);
@@ -36,9 +35,9 @@ before(function (beforeDone) {
 
 describe('proxy', function() {
   it('should not cache metadata requests', function(itDone) {
-    performNpmRequest(function (req, res, resBody) {
+    performNpmRequest(function(req, res, resBody) {
       var originalBody = resBody;
-      performNpmRequest(function (req, res, resBody) {
+      performNpmRequest(function(req, res, resBody) {
         assert.notDeepEqual(resBody, originalBody);
         assert.equal(calledMock['GET/test'], 2, 'Mock is not called twice');
         itDone();
@@ -49,11 +48,11 @@ describe('proxy', function() {
 
 function performNpmRequest(fn) {
   request({
-      baseUrl: 'http://127.0.0.1:' + APP_PORT,
-      uri: '/test',
-      headers: {
-        accept: 'application/json',
-        host: '127.0.0.1:' + MOCK_PORT
-      }
+    baseUrl: 'http://127.0.0.1:' + APP_PORT,
+    uri: '/test',
+    headers: {
+      accept: 'application/json',
+      host: '127.0.0.1:' + MOCK_PORT
+    }
   }, fn);
-};
+}
